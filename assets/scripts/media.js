1b0a2974dd616fa43ab704323de29d3f5a669ee2
Sanjib Roy <sanjibroy0098@gmail.com>
2020-03-25 19:40:44 +0530

    Complete

---
+++ assets/scripts/media.js
@@ -0,0 +1,30 @@
+
+function handleKeyPress(event) {
+    let audio = document.querySelector(`audio[data-key = "${event.keyCode}"]`);
+    let allKeys = document.querySelectorAll('.playing');
+    
+    if(audio) {
+        audio.currentTime = 0;  
+        audio.play();
+
+        let key = document.querySelector(`.key[data-key = "${event.keyCode}"]`);
+        
+       
+        allKeys.forEach(el => el.classList.remove('playing'));
+        key.classList.add('playing');
+    }
+
+    function removePlaying(event) {
+        if(event.propertyName == 'transform') {
+            event.target.classList.remove('playing');
+        }
+
+    }
+    allKeys.forEach(key => key.addEventListener('transitionend',removePlaying));
+
+        
+}
+
+
+
+document.addEventListener('keydown',handleKeyPress);
